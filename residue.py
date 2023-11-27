718699fc5f975d6b404e8134a7144c3abadfd495
lightediand <lightediand@mail.com>
2023-11-27 22:09:04 +0000

    determines set of nonresidues

--- residue.py
+++ residue.py
@@ -1,10 +1,14 @@
 from primes import primes
-p = primes(10)
+number = input("Choose an upper bound: ")
+p = primes(int(number))
 for prime in p:
     residues = set() 
+    nonresidues = set(range(1,prime))
     for i in range(1,prime):
         q = i**2
         i = q
         while i - prime >= 0:
             i = i - prime
         residues.add(i)
+        nonresidues.discard(i)
+    print(prime, residues, nonresidues)
